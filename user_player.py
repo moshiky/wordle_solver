8323aa2dd9a189984bb63b18f9daea9c8df2883a
Moshe Cohen <moshec40@gmail.com>
2022-07-06 17:53:28 +0300

    * add input validation

--- user_player.py
+++ user_player.py
@@ -7,9 +7,15 @@ class UserPlayer(WordlePlayer):
     def guess(self):
         log('Please enter your guess:')
         user_guess = input()
-        while not self._is_input_valid(user_guess):
-            log('Input invalid, please try again.')
+        while not self._is_input_valid(user_guess) or not self._is_valid_word(user_guess):
+            if not self._is_input_valid(user_guess):
+                log('Input invalid, please try again.')
+
+            if not self._is_valid_word(user_guess):
+                log('Word not in word list, please try again.')
+
             user_guess = input()
+
         return user_guess.upper()
 
     def init(self):
@@ -18,6 +24,9 @@ class UserPlayer(WordlePlayer):
     @staticmethod
     def _is_input_valid(user_input: str):
         return user_input.isalpha() and len(user_input) == 5
+
+    def _is_valid_word(self, user_input: str):
+        return user_input in self._word_list
 
     def update(self, judgment: str):
         # nothing to do
