6d838d5c50bdece624cfefd79b2bc5535378b5cc
Moshe Cohen <moshec40@gmail.com>
2022-07-06 18:52:18 +0300

    * add log message

--- auto_player.py
+++ auto_player.py
@@ -1,9 +1,11 @@
+from log_utils import log
 from wordle_player import WordlePlayer
 
 
 class AutoPlayer(WordlePlayer):
 
     def guess(self, solver_tip: str = None):
+        log(f'My guess is: {solver_tip}')
         return solver_tip
 
     def init(self):
